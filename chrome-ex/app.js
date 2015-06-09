143ebafe049164cbce11b4445d61effcaa8eb486
Shaun Xu <jfarrio@gmail.com>
2015-06-09 16:47:10 +0800

    a

--- chrome-ex/app.js
+++ chrome-ex/app.js
@@ -4,24 +4,113 @@
     var client_id = '54599295762c424b8aced6e7ee891a47';
     var return_url = chrome.identity.getRedirectURL();
 
-    var app = window.angular.module('Worktile', ['ngMaterial']);
+    var app = window.angular.module('Worktile', ['ngMessages', 'ngMaterial']);
 
     app.value('$', window.$);
 
     app.factory('$box', function () {
         return {
-            token: {}
+            token: {},
+            user: {},
+            projects: {},
+            isLoggedIn: function () {
+                var self = this;
+                return self.token && 
+                       self.token.access_token &&
+                       self.token.expires_in &&
+                       self.token.refresh_token;
+            },
+            clear: function () {
+                var self = this;
+                self.token = {};
+                self.user = {};
+            }
         }
     });
 
-    app.controller('MasterController', function ($scope, $http, $, $box) {
-        $scope.name = 'Shaun';
+    app.controller('MasterController', function ($scope, $http, $q, $, $box) {
+        $scope.loggedIn = !!$box.isLoggedIn();
 
         var _getParameterByName = function (url, name) {
             name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
             var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
                 results = regex.exec(url);
             return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+        };
+
+        var _loadCurrentUser = function (callback) {
+            $http({
+                method: 'GET',
+                url: 'https://api.worktile.com/v1/user/profile',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'access_token': $box.token.access_token
+                }
+            }).
+            success(function (data, status) {
+                $box.user = data;
+                return callback(null, $box.user);
+            }).
+            error(function (data, status) {
+                return callback({
+                    data: data,
+                    status: status
+                }, null);
+            });
+        };
+
+        var _loadMembersByProjectID = function (project, callback) {
+            $http({
+                method: 'GET',
+                url: 'https://api.worktile.com/v1/projects/' + project.pid + '/members',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'access_token': $box.token.access_token
+                }
+            }).
+            success(function (data, status) {
+                project.members = {};
+                window.angular.forEach(data, function (member) {
+                    // only dealing with members with normal status
+                    if (member.status === 1) {
+                        project.members[member.uid] = member;
+                    }
+                });
+                return callback(null, project.members);
+            }).
+            error(function (data, status) {
+                return callback({
+                    data: data,
+                    status: status
+                }, null);
+            });
+        };
+
+        var _loadProjects = function (callback) {
+            $http({
+                method: 'GET',
+                url: 'https://api.worktile.com/v1/projects',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'access_token': $box.token.access_token
+                }
+            }).
+            success(function (data, status) {
+                $box.projects = {};
+                window.angular.forEach(data, function (project) {
+                    // only working with unarchived projects
+                    if (project.archived === 0) {
+                        $box.projects[project.pid] = project;
+                    }
+                });
+                return callback(null, $box.projects);
+            }).
+            error(function (data, status) {
+                return callback({
+                    data: data,
+                    status: status
+                }, null);
+            });
         };
 
         $scope.login = function () {
@@ -31,15 +120,43 @@
             }, function (responseUrl) {
                 var code = _getParameterByName(responseUrl, 'code');
                 if (code) {
-                    $.ajax({
-                        type: 'POST',
+                    $http({
+                        method: 'POST',
                         url: 'https://api.worktile.com/oauth2/access_token',
-                        data: 'client_id=' + client_id + '&code=' + code,
-                        dataType: 'text',
-                        success: function (data, status) {
-                            var json = JSON.parse(data);
-                            $box.token = json;
+                        data: {
+                            client_id: client_id,
+                            code: code
                         }
+                    }).
+                    success(function (data, status) {
+                        $box.token = data;
+                        _loadCurrentUser(function (error, user) {
+                            if (error) {
+                                $box.clear();
+                                alert(JSON.stringify(error, null, 2));
+                            }
+                            else {
+                                _loadProjects(function (error, projects) {
+                                    if (error) {
+                                        $box.clear();
+                                        alert(JSON.stringify(error, null, 2));
+                                    }
+                                    else {
+                                        $scope.user = $box.user;
+                                        $scope.projects = $box.projects;
+                                        $scope.loggedIn = $box.isLoggedIn();
+                                    }
+                                });
+                            }
+                        });
+                    }).
+                    error(function (data, status) {
+                        $box.clear();
+                        alert(JSON.stringify({
+                            data: data,
+                            status: status
+                        }, null, 2));
+                        $scope.loggedIn = $box.isLoggedIn();
                     });
                 }
             });

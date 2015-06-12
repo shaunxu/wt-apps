dcafb29b5dd8918f79d573a9d5e33f2b987958d3
Shaun Xu <jfarrio@gmail.com>
2015-06-12 16:55:19 +0800

    finished (very simple version)

--- chrome-ex/app.js
+++ chrome-ex/app.js
@@ -4,74 +4,65 @@
     var client_id = '54599295762c424b8aced6e7ee891a47';
     var return_url = chrome.identity.getRedirectURL();
 
-    var app = angular.module('Worktile', ['ngMessages']);
+    var app = angular.module('Worktile', ['ngMessages', 'ngWorktile']);
 
-    app.value('$', $);
-
-    app.factory('$box', function () {
-        return {
-            token: {},
-            user: {},
-            projects: {},
-            isLoggedIn: function () {
-                var self = this;
-                return self.token && 
-                       self.token.access_token &&
-                       self.token.expires_in &&
-                       self.token.refresh_token;
-            },
-            clear: function () {
-                var self = this;
-                self.token = {};
-                self.user = {};
-            }
-        }
-    });
-
-    app.controller('MasterController', function ($scope, $http, $q, $, $box) {
-        $scope.post = {};
-
-        var _getParameterByName = function (url, name) {
-            name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-            var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-                results = regex.exec(url);
-            return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+    app.controller('MasterController', function ($scope, $http, $q, $worktile) {
+        $scope.name = $worktile.name;
+        $scope.post = {
+            title: null,
+            content: null,
+            pid: null,
+            followers: []
         };
 
-        var _loadCurrentUser = function (access_token, callback) {
-            $http({
-                method: 'GET',
-                url: 'https://api.worktile.com/v1/user/profile',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'access_token': access_token
-                }
-            }).
-            success(function (data, status) {
-                return callback(null, data);
-            }).
-            error(function (data, status) {
-                return callback({
-                    data: data,
-                    status: status
-                }, null);
-            });
+        $scope.me = $worktile.getCurrentUser() || {};
+
+        var lastProject = $worktile.getLastProject();
+        if (lastProject) {
+            $scope.post.pid = lastProject.pid;
+            $scope.projects = {};
+            $scope.projects[lastProject.pid] = lastProject;
+        }
+
+        var _showError = function (error) {
+            alert(angular.toJson(error, true));
+        };
+
+        var _showInformation = function (info) {
+            alert(angular.toJson(info, true));
         };
 
         $scope.$watch('post.pid', function (pid) {
-            $scope.post.followers = [];
-            if (pid && $scope.projects[pid]) {
-                localStorage.pid = pid;
-                _tryLoadProjectMembers(JSON.parse(localStorage.token).access_token, $scope.projects[pid], function (error, members) {
-                    if (error) {
-                        alert(JSON.stringify(error, null, 2));
-                    }
-                    else {
-                        $scope.members = members;
-                    }
-                });
+            var project = $scope.projects[pid];
+            if (project) {
+                if (project.members) {
+                    $worktile.setLastProject(project);
+                }
+                else {
+                    $worktile.getProjectMembersPromise(project)
+                        .then(function () {
+                            $worktile.setLastProject(project);
+                        })
+                        .catch(function (error) {
+                            _showError(error);
+                        });
+                }
             }
         });
+
+        if ($worktile.isLoggedIn()) {
+            $worktile.getProjectsPromise()
+                .then(function (projects) {
+                    $scope.projects = projects;
+                    $scope.post.pid = $worktile.getLastProject().pid;
+                    // if (!$scope.projects[$scope.post.pid].member) {
+                    //     $worktile.getProjectMembersPromise($scope.projects[$scope.post.pid]);
+                    // }
+                })
+                .catch(function (error) {
+                    _showError(error);
+                });
+        }
 
         $scope.copy = function () {
             chrome.tabs.query({
@@ -86,167 +77,75 @@
             });
         };
 
-        var _tryLoadProjectMembers = function (access_token, project, callback) {
-            if (project.members) {
-                return callback(null, project.members);
-            }
-            else {
-                $http({
-                    method: 'GET',
-                    url: 'https://api.worktile.com/v1/projects/' + project.pid + '/members',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'access_token': access_token
-                    }
-                }).
-                success(function (data, status) {
-                    project.members = {};
-                    angular.forEach(data, function (member) {
-                        // only dealing with members with normal status
-                        if (member.status === 1) {
-                            project.members[member.uid] = member;
-                        }
-                    });
-                    return callback(null, project.members);
-                }).
-                error(function (data, status) {
-                    return callback({
-                        data: data,
-                        status: status
-                    }, null);
-                });
-            }
-        };
-
-        var _tryLoadProjects = function (access_token, callback) {
-            if ($scope.projects) {
-                return callback(null, $scope.projects);
-            }
-            else {
-                $http({
-                    method: 'GET',
-                    url: 'https://api.worktile.com/v1/projects',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'access_token': access_token
-                    }
-                }).
-                success(function (data, status) {
-                    $scope.projects = {};
-                    $scope.post.pid = null;
-                    if (data && data.length > 0) {
-                        var i = 0;
-                        while (i <= data.length - 1) {
-                            // only dealing with non-archived projects
-                            if (data[i].archived === 0) {
-                                $scope.projects[data[i].pid] = data[i];
-                                if (data[i].pid === localStorage.pid) {
-                                    $scope.post.pid = localStorage.pid;
-                                }
-                            }
-                            i++;
-                        }
-                        if (!$scope.post.pid) {
-                            $scope.post.pid = $scope.projects[Object.getOwnPropertyNames($scope.projects)[0]].pid;
-                        }
-                    }
-                    return callback(null, $scope.projects);
-                }).
-                error(function (data, status) {
-                    return callback({
-                        data: data,
-                        status: status
-                    }, null);
-                });
-            }
-        };
-
-        var _isLoggedIn = function (callback) {
-            if (localStorage.token) {
-                var token = JSON.parse(localStorage.token);
-                if (token.access_token && token.expires_in && token.refresh_token) {
-                    _loadCurrentUser(token.access_token, function (error, user) {
-                        if (error) {
-                            return callback(null);
-                        }
-                        else {
-                            return callback(user);
-                        }
-                    });
-                }
-                else {
-                    return callback(null);
-                }
-            }
-            else {
-                return callback(null);
-            }
-        };
-
-        var _tryLogIn = function (callback) {
-            _isLoggedIn(function (user) {
-                if (user) {
-                    $scope.me = user;
-                    return callback(null, user);
-                }
-                else {
-                    chrome.identity.launchWebAuthFlow({
-                        url: 'https://open.worktile.com/oauth2/authorize?client_id=' + client_id + '&redirect_uri=' + return_url + '&display=mobile',
-                        interactive: true
-                    }, function (responseUrl) {
-                        var code = _getParameterByName(responseUrl, 'code');
-                        if (code) {
-                            $http({
-                                method: 'POST',
-                                url: 'https://api.worktile.com/oauth2/access_token',
-                                data: {
-                                    client_id: client_id,
-                                    code: code
-                                }
-                            }).
-                            success(function (data, status) {
-                                localStorage.token = angular.toJson(data);
-                                _loadCurrentUser(data.access_token, function (error, user) {
-                                    if (error) {
-                                        return callback(error);
-                                    }
-                                    else {
-                                        $scope.me = user;
-                                        return callback(null, user);
-                                    }
-                                });
-                            }).
-                            error(function (data, status) {
-                                var error = {
-                                    data: data,
-                                    status: status
-                                };
-                                return callback(error, null);
-                            });
-                        }
-                        else {
-                            return callback('Invalid code from Worktile Open API. Response URL = [' + responseUrl + '].', null);
-                        }
-                    });
-                }
-            });
-        };
-
         $scope.login = function () {
-            _tryLogIn(function (error) {
-                if (error) {
-                    alert(JSON.stringify(error, null, 2));
-                }
-                else {
-                    _tryLoadProjects(JSON.parse(localStorage.token).access_token, function (error, projects) {
-                        if (error) {
-                            alert(JSON.stringify(error, null, 2));
-                        }
-                    });
-                }
-            });
+            $worktile.logInPromise()
+                .then(function () {
+                    return $worktile.getCurrentUserPromise();
+                })
+                .then(function (user) {
+                    $scope.me = user;
+                    return $worktile.getProjectsPromise();
+                })
+                .then(function (projects) {
+                    $scope.projects = projects;
+                    $scope.post.pid = $worktile.getLastProject().pid;
+                })
+                .catch(function (error) {
+                    _showError(error);
+                });
         };
 
+        $scope.logout = function () {
+
+        };
+
+        $scope.submit = function () {
+            if (!$scope.post.title) {
+                _showError('Post title is required.');
+                return;
+            }
+            if (!$scope.post.pid) {
+                _showError('Project is required.');
+                return;
+            }
+
+            $worktile.createPostPromise($scope.post)
+                .then(function (post) {
+                    if ($scope.post.followers.length > 0) {
+                        return $worktile.addPostFollowersPromise(post.pid, post.post_id, $scope.post.followers || []);
+                    }
+                    else {
+                        return {
+                           success: true
+                        };
+                    }
+                })
+                .then(function (result) {
+                    _showInformation('Post submitted successfully.');
+                    $scope.post.title = null;
+                    $scope.post.content = null;
+                    window.close();
+                })
+                .catch(function (error) {
+                    _showError(error);
+                });
+        };
+
+        $scope.refresh = function () {
+            $worktile.getCurrentUserPromise()
+                .then(function (user) {
+                    $scope.me = user;
+                    return $worktile.getProjectsPromise();
+                })
+                .then(function (projects) {
+                    $scope.projects = projects;
+                    $scope.post.pid = $worktile.getLastProject().pid;
+                    return $worktile.getProjectMembersPromise($scope.projects[$scope.post.pid]);
+                })
+                .catch(function (error) {
+                    _showError(error);
+                });
+        };
     });
 
 })();
---
+++ chrome-ex/worktile.js
@@ -0,0 +1,247 @@
+(function () {
+    var worktile = angular.module('ngWorktile', []);
+
+    worktile.value('$clientId', '54599295762c424b8aced6e7ee891a47');
+    worktile.value('$redirectURL', chrome.identity.getRedirectURL());
+
+    worktile.factory('$worktile', function ($http, $interval, $q, $clientId, $redirectURL) {
+        var _getParameterByName = function (url, name) {
+            name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+            var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+                results = regex.exec(url);
+            return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+        };
+
+        var _storage = {
+            get: function (key) {
+                var raw = localStorage[key];
+                var result = null;
+                if (raw) {
+                    try {
+                        result = angular.fromJson(raw);
+                    }
+                    catch (ex) {}
+                }
+                return result;
+            },
+            set: function (key, value) {
+                if (value) {
+                    var raw = angular.toJson(value, false);
+                    localStorage[key] = raw;
+                }
+                else {
+                    delete localStorage[key];
+                }
+            },
+        };
+
+        var _interval = null;
+
+        return {
+            name: 'Worktile',
+            getToken: function () {
+                return _storage.get('token');
+            },
+            setToken: function (token) {
+                _storage.set('token', token);
+            },
+            getCurrentUser: function () {
+                return _storage.get('me');
+            },
+            setCurrentUser: function (me) {
+                _storage.set('me', me);
+            },
+            getLastProject: function () {
+                return _storage.get('lastPorject');
+            },
+            setLastProject: function (project) {
+                _storage.set('lastPorject', project);
+            },
+            startRefreshAccessToken: function () {
+                var self = this;
+
+            },
+            stopRefreshAccessToken: function () {
+                var self = this;
+
+            },
+            isLoggedIn: function () {
+                var self = this;
+                var token = self.getToken();
+                var me = self.getCurrentUser();
+                var lastProject = self.getLastProject();
+                return ( 
+                    token && token.access_token && token.expires_in && token.refresh_token &&
+                    me && me.uid &&
+                    lastProject && lastProject.pid
+                );
+            },
+            getProjectMembersPromise: function (project) {
+                var self = this;
+                return $q(function (resolve, reject) {
+                    $http({
+                        method: 'GET',
+                        url: 'https://api.worktile.com/v1/projects/' + project.pid + '/members',
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'access_token': self.getToken().access_token
+                        }
+                    }).then(function (response) {
+                        project.members = {};
+                        angular.forEach(response.data, function (member) {
+                            // only dealing with members with normal status
+                            if (member.status === 1) {
+                                project.members[member.uid] = member;
+                            }
+                        });
+                        return resolve(null, project.members);
+                    }, function (response) {
+                        return reject(response);
+                    });
+                });
+            },
+            getProjectsPromise: function () {
+                var self = this;
+                return $q(function (resolve, reject) {
+                    $http({
+                        method: 'GET',
+                        url: 'https://api.worktile.com/v1/projects',
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'access_token': self.getToken().access_token
+                        }
+                    }).then(function (response) {
+                        var projects = response.data;
+                        var result = {};
+                        if (projects.length > 0) {
+                            var lastPorject = self.getLastProject() || {};
+                            var matched = false;
+                            angular.forEach(projects, function (project) {
+                                if (project.archived === 0) {
+                                    if (project.pid === lastPorject.pid) {
+                                        matched = true;
+                                    }
+                                    result[project.pid] = project;
+                                }
+                            });
+                            if (matched) {
+                                result[lastPorject.pid].members = lastPorject.members;
+                                return resolve(result);
+                            }
+                            else {
+                                lastPorject = projects[0];
+                                self.getProjectMembersPromise(lastPorject)
+                                    .then(function () {
+                                        self.setLastProject(lastPorject);
+                                        return resolve(result);
+                                    })
+                                    .catch(function (error) {
+                                        return reject(error);
+                                    });
+                            }
+                        }
+                        else {
+                            self.setLastProject(null);
+                            return resolve(result);
+                        }
+                    }, function (response) {
+                        return reject(response);
+                    });
+                });
+            },
+            getCurrentUserPromise: function () {
+                var self = this;
+                return $q(function (resolve, reject) {
+                    $http({
+                        method: 'GET',
+                        url: 'https://api.worktile.com/v1/user/profile',
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'access_token': self.getToken().access_token
+                        }
+                    }).then(function (response, status) {
+                        self.setCurrentUser(response.data);
+                        return resolve(response.data);
+                    }, function (response) {
+                        return reject(response);
+                    });
+                });
+            },
+            logInPromise: function () {
+                var self = this;
+                return $q(function (resolve, reject) {
+                    chrome.identity.launchWebAuthFlow({
+                        url: 'https://open.worktile.com/oauth2/authorize?client_id=' + $clientId + '&redirect_uri=' + $redirectURL + '&display=mobile',
+                        interactive: true
+                    }, function (responseUrl) {
+                        var code = _getParameterByName(responseUrl, 'code');
+                        if (code) {
+                            $http({
+                                method: 'POST',
+                                url: 'https://api.worktile.com/oauth2/access_token',
+                                data: {
+                                    client_id: $clientId,
+                                    code: code
+                                }
+                            }).then(function (response) {
+                                self.setToken(response.data);
+                                return resolve(response.data);
+                            }, function (response) {
+                                return reject(response);
+                            });
+                        }
+                        else {
+                            return reject('Invalid code from Worktile Open API. Response URL = [' + responseUrl + '].');
+                        }
+                    });
+                });
+            },
+            addPostFollowersPromise: function (pid, postId, uids) {
+                var self = this;
+                return $q(function (resolve, reject) {
+                    $http({
+                        method: 'POST',
+                        url: 'https://api.worktile.com/v1/posts/' + postId + '/watcher?pid=' + pid,
+                        headers: {
+                            // 'Content-Type': 'application/json',
+                            'access_token': self.getToken().access_token
+                        },
+                        data: {
+                            uids: uids
+                        }
+                    }).then(function (response, status) {
+                        return resolve(response.data);
+                    }, function (response) {
+                        return reject(response);
+                    });
+                });
+            },
+            createPostPromise: function (post) {
+                var self = this;
+                return $q(function (resolve, reject) {
+                    if (post && post.pid && post.title) {
+                        $http({
+                            method: 'POST',
+                            url: 'https://api.worktile.com/v1/post?pid=' + post.pid,
+                            headers: {
+                                // 'Content-Type': 'application/json',
+                                'access_token': self.getToken().access_token
+                            },
+                            data: {
+                                name: post.title,
+                                content: post.content
+                            }
+                        }).then(function (response, status) {
+                            return resolve(response.data);
+                        }, function (response) {
+                            return reject(response);
+                        });
+                    }
+                    else {
+                        return reject('Invalid post value. ' + angular.toJson(post));
+                    }
+                });
+            }
+        };
+    });
+})();
